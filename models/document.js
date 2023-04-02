e38658ac507df57e3320119d84a9b91e3d9ccbea
Lava Kedar <lavkedar1298@gmail.com>
2023-04-02 20:07:13 +0530

    something

---
+++ models/document.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const documentSchema = mongoose.Schema({
+  value: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Document", documentSchema);
